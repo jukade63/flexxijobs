5cb6410049d5c337802ab97438b237687147ead4
jukade63 <jukade63@gmail.com>
2024-04-12 00:36:37 +0700

    style global-error page

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -1,18 +1,20 @@
-'use client'
- 
+"use client";
+
 export default function GlobalError({
   error,
   reset,
 }: {
-  error: Error & { digest?: string }
-  reset: () => void
+  error: Error & { digest?: string };
+  reset: () => void;
 }) {
   return (
     <html>
       <body>
-        <h2>Something went wrong!</h2>
-        <button onClick={() => reset()}>Try again</button>
+        <div className="flex justify-center items-center h-screen w-screen">
+          <h2>Something went wrong!</h2>
+          <button onClick={() => reset()}>Try again</button>
+        </div>
       </body>
     </html>
-  )
+  );
 }
